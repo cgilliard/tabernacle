9b5bcf0f185bb12d2bb21b0d6bb33ad39f75f40e
Christopher Gilliard <christopher.gilliard@gmail.com>
2026-06-13 15:32:29 -0700

    extract syscall

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+mod syscall;
+
+use core::panic::PanicInfo;
+
+#[link(name = "c")]
+unsafe extern "C" {}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn main() -> i32 {
+    syscall::write(1, b"Hello, world!\n");
+    0
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    syscall::exit(1)
 }
