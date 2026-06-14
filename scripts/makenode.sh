f06d7b9c7909f9ceaeb360ecf83ce6c6972170f1
Christopher Gilliard <christopher.gilliard@gmail.com>
2026-06-13 19:23:13 -0700

    makenode/node

---
+++ scripts/makenode.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -e
+IMG=./data/disk.img
+dd if=/dev/zero of="$IMG" bs=1M count=8 2>/dev/null
+dd if=bin/full_node.bin of="$IMG" conv=notrunc 2>/dev/null
+echo "$IMG: $(stat -c %s bin/full_node.bin) bytes written"
