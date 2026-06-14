2b60c96940f07da33d37580bc749511a2e1a0589
Christopher Gilliard <christopher.gilliard@gmail.com>
2026-06-13 18:18:37 -0700

    tabernacle

--- scripts/build.sh
+++ scripts/build.sh
@@ -76,14 +76,14 @@ run bin/fampack bin/fam.uncompressed lib/stdlib.fam lib/asm.fam src/fampack.fam
 pack bin/fam.uncompressed bin/fam
 run bin/gen_hash bin/fam lib/stdlib.fam lib/asm.fam src/gen_hash.fam
 run bin/tabernacle.uncompressed bin/fam0 src/tabernacle.fam0
-#run bin/full_node.bin \
-#	bin/fam \
-#	lib/stdlib.fam \
-#	lib/build.fam \
-#	src/node_disk.fam \
-#	src/node_serve.fam \
-#	src/full_node.fam
-#cat resources/bible.compressed >> bin/full_node.bin
-#patch_config bin/tabernacle.uncompressed bin/full_node.bin
-#pack bin/tabernacle.uncompressed bin/tabernacle
+run bin/full_node.bin \
+	bin/fam \
+	lib/stdlib.fam \
+	lib/build.fam \
+	src/node_disk.fam \
+	src/node_serve.fam \
+	src/full_node.fam
+cat resources/bible.compressed >> bin/full_node.bin
+patch_config bin/tabernacle.uncompressed bin/full_node.bin
+pack bin/tabernacle.uncompressed bin/tabernacle
 
